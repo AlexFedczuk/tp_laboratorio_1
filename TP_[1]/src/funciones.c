85bb5647a60243ff092ce33c18b918ccc123b2a6
AlexFedczuk <alexfed1011@gmail.com>
2022-06-29 12:25:14 -0300

    fix(): remove and add spaces

--- TP_[1]/src/funciones.c
+++ TP_[1]/src/funciones.c
@@ -1,17 +1,21 @@
 #include "funciones.h"
 
 void mostrarMenu(int opcionUnoCargada, int opcionDosCargada, float kmIngresados, float precioAereolineas, float precioLatam){
+	
     printf("Menu pricipal\n");
+	
     if(opcionUnoCargada == 0){
-        printf("1. Ingresar Kilómetros (km = x)\n");// Cuidado con las tildes.
+        printf("1. Ingresar KilÃ³metros (km = x)\n");// Cuidado con las tildes.
     }else{
-        printf("1. Ingresar Kilómetros (km = %.2f)\n", kmIngresados);// Cuidado con las tildes.
+        printf("1. Ingresar KilÃ³metros (km = %.2f)\n", kmIngresados);// Cuidado con las tildes.
     }
+	
     if(opcionDosCargada == 0){
-        printf("2. Ingresar Precio de Vuelos (Aerolíneas = y, Latam = z)\n");// Cuidado con las tildes.
+        printf("2. Ingresar Precio de Vuelos (AerolÃ­neas = y, Latam = z)\n");// Cuidado con las tildes.
     }else{
-        printf("2. Ingresar Precio de Vuelos (Aerolíneas = %.2f, Latam = %.2f)\n", precioAereolineas, precioLatam);// Cuidado con las tildes.
+        printf("2. Ingresar Precio de Vuelos (AerolÃ­neas = %.2f, Latam = %.2f)\n", precioAereolineas, precioLatam);// Cuidado con las tildes.
     }
+	
     printf("3. Calcular todos los costos\n");
     printf("4. Informar Resultados\n");
     printf("5. Carga forzada de datos\n"); // Esta opcion ejecuta el CALCULO y el INFORME.
@@ -63,15 +67,15 @@ void informarCargaForzada(
 		float diferencia)
 {
 	printf("\nKMs Ingresados: %.2f km\n\n", kmIngresados);
-    printf("Precio Aerolineas: $%.2f\n", precioAereolineas);
-    printf("a) Precio con tarjeta de debito: $ %.2f\n", aereolineasDebito);
-    printf("b) Precio con tarjeta de credito: $ %.2f\n", aereolineasCredito);
-    printf("c) Precio pagando con bitcoin: %.2f BTC\n", aereolineasBitcoin);
-    printf("d) Precio unitario: %.2f\n\n", aereolineasUnitario);
-    printf("\nPrecio Latam: $%.2f\n", precioLatam);
-    printf("a) Precio con tarjeta de debito: $ %.2f\n", latamDebito);
-    printf("b) Precio con tarjeta de credito: $ %.2f\n", latamCredito);
-    printf("c) Precio pagando con bitcoin : %.2f BTC\n", latamBitcoin);
-    printf("d) Precio unitario: $ %.2f\n\n", latamUnitario);
-    printf("La diferencia de precio es: $ %.2f\n\n", diferencia);
+    	printf("Precio Aerolineas: $%.2f\n", precioAereolineas);
+    	printf("a) Precio con tarjeta de debito: $ %.2f\n", aereolineasDebito);
+   	printf("b) Precio con tarjeta de credito: $ %.2f\n", aereolineasCredito);
+   	printf("c) Precio pagando con bitcoin: %.2f BTC\n", aereolineasBitcoin);
+    	printf("d) Precio unitario: %.2f\n\n", aereolineasUnitario);
+    	printf("\nPrecio Latam: $%.2f\n", precioLatam);
+    	printf("a) Precio con tarjeta de debito: $ %.2f\n", latamDebito);
+    	printf("b) Precio con tarjeta de credito: $ %.2f\n", latamCredito);
+    	printf("c) Precio pagando con bitcoin : %.2f BTC\n", latamBitcoin);
+    	printf("d) Precio unitario: $ %.2f\n\n", latamUnitario);
+    	printf("La diferencia de precio es: $ %.2f\n\n", diferencia);
 }
