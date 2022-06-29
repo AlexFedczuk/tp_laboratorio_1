0920f7de71cb41dc9ca93b52fb3d974925fe56c2
AlexFedczuk <alexfed1011@gmail.com>
2022-06-29 12:40:34 -0300

    fix(): fix documentation, and add and remove spaces

--- TP_[1]/src/inputFuntions.h
+++ TP_[1]/src/inputFuntions.h
@@ -4,6 +4,9 @@
  *  Created on: Apr 11, 2022
  *      Author: Alex Yago Fedczuk
  */
+#ifndef INPUT_FUNTIONS_H_
+#define INPUT_FUNTIONS_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,47 +15,39 @@
 
 #define BUFFER_SIZE 4096
 
-#ifndef INPUT_FUNTIONS_H_
-#define INPUT_FUNTIONS_H_
-
-// Menues
 /**
   @brief Esta funcion se encarga de validar que se ingreso un valor de tipo entero por consola.
-  @param ...
-  @param ...
-  @param ...
+  @param char *string, el valor como cadena.
+  @param int *integer, el valor pasado a numero entero.
   @returns Deveuelve un valor de tipo Booleano.
 */
 bool parseInt(char *string, int *integer);
+
 /**
   @brief Esta funcion se encarga de validar que se ingreso un valor de tipo flotante por consola.
-  @param ...
-  @param ...
-  @param ...
+  @param char *string, el valor como cadena.
+  @param float *nfloat, el valor pasado a numero flotante.
   @returns Deveuelve un valor de tipo Booleano.
 */
 bool parseFloat(char *string, float *nfloat);
+
 /**
   @brief Esta funcion se encarga de pedir por consola el ingreso de un valor de tipo flotante.
-  @param ...
-  @param ...
-  @param ...
+  @param char mensaje[], mensaje a mostrar al usuario.
   @returns Deveuelve un valor de tipo Flotante.
 */
 float pedirNumeroFlotante(char mensaje[]);
+
 /**
   @brief Esta funcion se encarga de pedir por consola el ingreso de un valor de tipo entero.
-  @param ...
-  @param ...
-  @param ...
+  @param char mensaje[], mensaje a mostrar al usuario.
   @returns Deveuelve un valor de tipo Entero.
 */
 int pedirNumeroEntero(char mensaje[]);
+
 /**
   @brief Esta funcion se encarga de pedir por consola el ingreso de un valor de tipo entero NO negativo.
-  @param ...
-  @param ...
-  @param ...
+  @param char mensaje[], mensaje a mostrar al usuario.
   @returns Deveuelve un valor de tipo Entero NO negativo.
 */
 float pedirValorNoNegativo(char mensaje[]);
